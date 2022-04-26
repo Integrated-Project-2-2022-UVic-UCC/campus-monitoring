99c686cedc7f51e46fa813aa6984c6f39273e21a
Roger Enguidanos Ruiz <roger.enguidanos@uvic.cat>
2022-04-26 19:32:02 +0200

    Actualitzacio codig wifi

--- Nodes/Wifi/ConexioWiFi/ConexioWiFi.ino
+++ Nodes/Wifi/ConexioWiFi/ConexioWiFi.ino
@@ -66,10 +66,10 @@ void loop() {
       HTTPClient http;
 
       http.begin(client, serverName);
-
+      
       // Specify content-type header
       http.addHeader("Content-Type", "application/json");       
-      int httpResponseCode = http.POST("{\"timestamp\":\"tPmAT5Ab3j7F9\",\"sensorID\":\"1\",\"var1\":\"24.25\",\"var2\":\"49.54\",\"var3\":\"1005.14\"\"var4\":\"24.25\",\"var5\":\"49.54\",\"var6\":\"1005.14\"\"var7\":\"24.25\"}");
+      int httpResponseCode = http.POST("{\"timestamp\":\"2020-04-26T08:45:12\",\"sensorID\":2,\"var1\":24.25,\"var2\":49.54,\"var3\":84.14,\"var4\":24.25,\"var5\":49.54\,\"var6\":145.14,\"var7\":24.25}");
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
         
@@ -86,16 +86,13 @@ void loop() {
 void setupDateTime() {
   // setup this after wifi connected
   // you can use custom timeZone,server and timeout
-  // DateTime.setTimeZone("CST-8");
-  // DateTime.setServer("asia.pool.ntp.org");
-  // DateTime.begin(15 * 1000);
-  // from
-  /** changed from 0.2.x **/
+  DateTime.setServer("http://192.168.0.100:8080/api/monitoring");
   DateTime.setTimeZone("CST-8");
-  // this method config ntp and wait for time sync
-  // default timeout is 10 seconds
-  DateTime.begin(/* timeout param */);
+  DateTime.begin();
   if (!DateTime.isTimeValid()) {
     Serial.println("Failed to get time from server.");
+  } else {
+    Serial.printf("Date Now is %s\n", DateTime.toISOString().c_str());
+    Serial.printf("Timestamp is %ld\n", DateTime.now());
   }
 }
