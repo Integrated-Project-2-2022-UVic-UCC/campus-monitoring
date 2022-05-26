dcf834c13bc29fb7ccbee8023f11b50bc51ea73c
Roger Enguidanos Ruiz <roger.enguidanos@uvic.cat>
2022-05-26 16:11:51 +0200

    Conexio wifi de tots els nodes

--- Nodes/Exterior/SensorTempHumitCo2/sketch_may17a/sketch_may17a.ino
+++ Nodes/Exterior/SensorTempHumitCo2/sketch_may17a/sketch_may17a.ino
@@ -1,5 +1,8 @@
 #include "DFRobot_BME280.h"
 #include "Wire.h"
+#include "DFRobot_CCS811.h"
+
+DFRobot_CCS811 CCS811;
 
 typedef DFRobot_BME280_IIC    BME;    // ******** use abbreviations instead of full names ********
 
@@ -19,8 +22,7 @@ void printLastOperateStatus(BME::eStatus_t eStatus)
   }
 }
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   bme.reset();
   Serial.println("bme read data test");
@@ -31,10 +33,15 @@ void setup()
   }
   Serial.println("bme begin success");
   delay(100);
+
+  while(CCS811.begin() != 0){
+        Serial.println("failed to init chip, please check if the chip connection is fine");
+        delay(1000);
+}
 }
 
-void loop()
-{
+void loop(){
+
   float   temp = bme.getTemperature();
   uint32_t    press = bme.getPressure();
   float   alti = bme.calAltitude(SEA_LEVEL_PRESSURE, press);
@@ -48,5 +55,23 @@ void loop()
   Serial.print("humidity (unit percent):    "); Serial.println(humi);
   Serial.println("========  end print  ========");
 
+  delay(1000);
+
+  if(CCS811.checkDataReady() == true){
+        Serial.print("CO2: ");
+        Serial.print(CCS811.getCO2PPM());
+        Serial.print("ppm, TVOC: ");
+        Serial.print(CCS811.getTVOCPPB());
+        Serial.println("ppb");
+
+    } else {
+        Serial.println("Data is not ready!");
+    }
+    /*!
+     * @brief Set baseline
+     * @param get from getBaseline.ino
+     */
+  CCS811.writeBaseLine(0x447B);
+    //delay cannot be less than measurement cycle
   delay(1000);
 } 
